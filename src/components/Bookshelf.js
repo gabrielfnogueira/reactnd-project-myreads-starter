c9019a365381e2e4d3ceef887887dc8fa355d41e
Gabriel Nogueira <gfnogueira@gmail.com>
2018-06-14 23:58:56 -0300

    fix: align loading to center

--- src/components/Bookshelf.js
+++ src/components/Bookshelf.js
@@ -1,17 +1,24 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import ReactLoading from 'react-loading';
 import Book from './Book';
 
-const Bookshelf = ({ title, books, moveBook }) => (
+const Bookshelf = ({ title, books, isLoading, moveBook }) => (
   <div className="bookshelf">
     <h2 className="bookshelf-title">{title}</h2>
     <div className="bookshelf-books">
       <ol className="books-grid">
-        {books.map(book => (
-          <li key={book.id}>
-            <Book data={book} moveBook={moveBook} />
-          </li>
-        ))}
+        {isLoading ? (
+          <div className="loading">
+            <ReactLoading type="spinningBubbles" color="#2e7c31" />
+          </div>
+        ) : (
+          books.map(book => (
+            <li key={book.id}>
+              <Book data={book} moveBook={moveBook} />
+            </li>
+          ))
+        )}
       </ol>
     </div>
   </div>
@@ -20,6 +27,7 @@ const Bookshelf = ({ title, books, moveBook }) => (
 Bookshelf.propTypes = {
   title: PropTypes.string.isRequired,
   books: PropTypes.array.isRequired,
+  isLoading: PropTypes.bool.isRequired,
   moveBook: PropTypes.func.isRequired
 };
 
